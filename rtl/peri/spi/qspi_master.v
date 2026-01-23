fc564d0748136b6f8972d798ddd00781c812b537
Sotatek-ThanhTran <thanh.tran@sotatek.com>
2026-01-23 19:32:37 +0700

    Improve instruction memory: prevent idle cycles

--- rtl/peri/spi/qspi_master.v
+++ rtl/peri/spi/qspi_master.v
@@ -29,8 +29,7 @@ module spi_master (
     parameter FSM_SEND_ADDR = 3'b011;
     parameter FSM_DUMMY = 3'b100;
     parameter FSM_DATA_TRANSFER = 3'b101;
-    parameter FSM_PAUSE = 3'b110;
-    parameter FSM_DONE = 3'b111;
+    parameter FSM_DONE = 3'b110;
 
     localparam INIT_CYCLES = 12'd4095;
 
@@ -51,8 +50,9 @@ module spi_master (
     wire [7:0] cmd = write_enable ? 8'h38 : 8'hEB;
     wire [31:0] cmd_addr = {cmd, addr};
 
-    wire is_compressed_instr = (bit_counter == 16) && (shift_reg_in[9:8] != 2'b11);
-    wire is_normal_instr = (bit_counter == 32);
+    // These conditions are checked right before receiving the last 4 bits of the instruction
+    wire is_compressed_instr = (bit_counter == 12) && (shift_reg_in[5:4] != 2'b11);
+    wire is_normal_instr = (bit_counter == 28);
     wire is_instr_complete = is_instr && (fsm_state == FSM_DATA_TRANSFER) && (is_compressed_instr || is_normal_instr);
 
     // State machine
@@ -116,25 +116,13 @@ module spi_master (
 
             FSM_DATA_TRANSFER: begin
                 if (is_instr) begin
-                    if (is_instr_complete) begin
-                        fsm_next_state = FSM_PAUSE;
-                    end else begin
-                        fsm_next_state = FSM_DATA_TRANSFER;
-                    end
+                    fsm_next_state = FSM_DATA_TRANSFER;
                 end else begin
                     if (bit_counter == data_len) begin
                         fsm_next_state = FSM_DONE;
                     end else begin
                         fsm_next_state = FSM_DATA_TRANSFER;
                     end
-                end
-            end
-
-            FSM_PAUSE: begin
-                if (cont) begin
-                    fsm_next_state = FSM_DATA_TRANSFER;
-                end else begin
-                    fsm_next_state = FSM_PAUSE;
                 end
             end
 
@@ -275,6 +263,7 @@ module spi_master (
                 end
 
                 FSM_DATA_TRANSFER: begin
+                    done <= 1'b0;
                     spi_clk_en <= 1'b1;
                     spi_cs_n <= 1'b0;
                     if (is_write_op) begin
@@ -291,46 +280,23 @@ module spi_master (
                         spi_io_out <= 4'b0000;      // Don't drive outputs
                         
                         if (spi_clk == 1'b0) begin  // Rising edge - sample input
-                            shift_reg_in <= {shift_reg_in[27:0], spi_io_in};  // Shift in 4 bits
-                            bit_counter <= bit_counter + 4;
-                        end
-                    end
-
-                    // In case of fetch instruction, we update some flag here to save one clock cycle
-                    if (is_instr_complete) begin
-                        spi_clk_en <= 1'b0;
-                        bit_counter <= 6'b0;
-                        done <= 1'b1;
-                        if (bit_counter == 16) begin
-                            data_out <= {shift_reg_in[15:0], 16'b0};
-                        end else begin
-                            data_out <= shift_reg_in;
+                            if (is_instr_complete) begin
+                            // In case of fetch instruction, we update flag and data here to save one clock cycle
+                                bit_counter <= 6'b0;
+                                done <= 1'b1;
+                                if (bit_counter == 12) begin
+                                    data_out <= {shift_reg_in[11:0], spi_io_in, 16'b0};
+                                end else begin
+                                    data_out <= {shift_reg_in[27:0], spi_io_in};
+                                end
+                            end else begin
+                                shift_reg_in <= {shift_reg_in[27:0], spi_io_in};  // Shift in 4 bits
+                                bit_counter <= bit_counter + 4;
+                            end
                         end
                     end
 
                     write_mosi <= ~write_mosi;
-                end
-
-                FSM_PAUSE: begin
-                    done <= 1'b0;
-                    spi_io_oe <= 4'b0000;
-                    spi_io_out <= 4'b0000;
-                    spi_clk_en <= 1'b0;
-                    bit_counter <= 6'b0;
-                    shift_reg_in <= 32'b0;
-                    shift_reg_out <= 32'b0;
-                    is_write_op <= 1'b0;
-                    
-                    if (cont) begin
-                        spi_clk_en <= 1'b1;
-                        // Read data immediately when cont flag is set to save 1 clock cycle
-                        if (spi_clk == 1'b0) begin  // Rising edge - sample input
-                            shift_reg_in <= {shift_reg_in[27:0], spi_io_in};
-                            bit_counter <= bit_counter + 4;
-                        end
-                        spi_clk <= 1'b1;
-                        write_mosi <= 1'b1;
-                    end
                 end
 
                 FSM_DONE: begin
