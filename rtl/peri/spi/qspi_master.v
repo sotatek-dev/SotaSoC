44dcdc8fb80b7ddfbbbed5497f7d8b52bbeb08b4
Sotatek-ThanhTran <thanh.tran@sotatek.com>
2026-01-24 22:29:43 +0700

    Enable QSPI RAM 4-4-4

--- rtl/peri/spi/qspi_master.v
+++ rtl/peri/spi/qspi_master.v
@@ -22,20 +22,30 @@ module spi_master (
 );
 
     // Parameters
-    parameter FSM_IDLE = 3'b000;
-    parameter FSM_INIT = 3'b001;
-    parameter FSM_RESET = 3'b010;
-    parameter FSM_SEND_CMD = 3'b011;
-    parameter FSM_SEND_ADDR = 3'b100;
-    parameter FSM_DUMMY = 3'b101;
-    parameter FSM_DATA_TRANSFER = 3'b110;
-    parameter FSM_DONE = 3'b111;
+    parameter FSM_IDLE = 4'b0000;
+    parameter FSM_INIT = 4'b0001;
+    parameter FSM_SEND_CMD = 4'b0010;
+    parameter FSM_SEND_CMD_QUAD = 4'b0011;
+    parameter FSM_SEND_ADDR = 4'b0100;
+    parameter FSM_DUMMY = 4'b0101;
+    parameter FSM_DATA_TRANSFER = 4'b0110;
+    parameter FSM_DONE = 4'b0111;
+
+    parameter FSM_RESET_FLASH0 = 4'b1000;
+    parameter FSM_RESET_FLASH1 = 4'b1001;
+    parameter FSM_RESET_FLASH2 = 4'b1010;
+    parameter FSM_RESET_FLASH3 = 4'b1011;
+    parameter FSM_RESET_FLASH4 = 4'b1100;
+
+    parameter FSM_RESET_RAM0 = 4'b1101;
+    parameter FSM_RESET_RAM1 = 4'b1110;
+    parameter FSM_RESET_RAM2 = 4'b1111;
 
     localparam INIT_CYCLES = 12'd4095;
 
     // Internal signals
-    reg [2:0] fsm_state;
-    reg [2:0] fsm_next_state;
+    reg [3:0] fsm_state;
+    reg [3:0] fsm_next_state;
     reg [5:0] bit_counter;               // Counts bits transferred
     reg [31:0] shift_reg_out;
     reg [31:0] shift_reg_in;
@@ -47,7 +57,10 @@ module spi_master (
     reg initialized;
     reg [11:0] init_cnt;
 
-    reg in_continuous_mode;
+    reg flash_in_cont_mode;
+    reg ram_in_quad_mode;
+
+    wire[7:0] cmd_enter_quad_mode = 8'hAC;
 
     wire [7:0] cmd = write_enable ? 8'h38 : 8'hEB;
     wire [31:0] cmd_addr = {cmd, addr};
@@ -72,10 +85,21 @@ module spi_master (
             FSM_IDLE: begin
                 if (start) begin
                     if (initialized) begin
-                        if (is_instr && in_continuous_mode) begin
-                            fsm_next_state = FSM_SEND_ADDR;
+                        if (is_instr) begin
+                            if (flash_in_cont_mode) begin
+                                fsm_next_state = FSM_SEND_ADDR;
+                            end else begin
+                                // This case should not happen
+                                // Flash shoule be in continuous mode after initialization
+                                fsm_next_state = FSM_SEND_CMD;
+                            end
                         end else begin
-                            fsm_next_state = FSM_SEND_CMD;
+                            if (ram_in_quad_mode) begin
+                                fsm_next_state = FSM_SEND_CMD_QUAD;
+                            end else begin
+                                fsm_next_state = FSM_RESET_RAM0;
+                            end
+                            // fsm_next_state = FSM_SEND_CMD;
                         end
 //                        `DEBUG_PRINT(("Time %0t: SPI_MASTER - Starting SPI transaction: cmd_addr=0x%h", $time, cmd_addr));
                     end else begin
@@ -89,17 +113,51 @@ module spi_master (
 
             FSM_INIT: begin
                 if (init_cnt == INIT_CYCLES) begin
-                    fsm_next_state = FSM_RESET;
+                    fsm_next_state = FSM_RESET_FLASH0;
                 end else begin
                     fsm_next_state = FSM_INIT;
                 end
             end
 
-            FSM_RESET: begin
-                if (bit_counter == 15)
-                    fsm_next_state = FSM_SEND_CMD;
+            FSM_RESET_FLASH0: begin
+                fsm_next_state = FSM_RESET_FLASH1;
+            end
+
+            FSM_RESET_FLASH1: begin
+                if (bit_counter == 7)
+                    fsm_next_state = FSM_RESET_FLASH2;
                 else
-                    fsm_next_state = FSM_RESET;
+                    fsm_next_state = FSM_RESET_FLASH1;
+            end
+
+            FSM_RESET_FLASH2: begin
+                if (bit_counter == 13)
+                    fsm_next_state = FSM_RESET_FLASH3;
+                else
+                    fsm_next_state = FSM_RESET_FLASH2;
+            end
+
+            FSM_RESET_FLASH3: begin
+                fsm_next_state = FSM_RESET_FLASH4;
+            end
+
+            FSM_RESET_FLASH4: begin
+                fsm_next_state = FSM_SEND_CMD;
+            end
+
+            FSM_RESET_RAM0: begin
+                fsm_next_state = FSM_RESET_RAM1;
+            end
+
+            FSM_RESET_RAM1: begin
+                if (bit_counter == 8)
+                    fsm_next_state = FSM_RESET_RAM2;
+                else
+                    fsm_next_state = FSM_RESET_RAM1;
+            end
+
+            FSM_RESET_RAM2: begin
+                fsm_next_state = FSM_SEND_CMD_QUAD;
             end
 
             FSM_SEND_CMD: begin
@@ -107,6 +165,13 @@ module spi_master (
                     fsm_next_state = FSM_SEND_ADDR;
                 else
                     fsm_next_state = FSM_SEND_CMD;
+            end
+
+            FSM_SEND_CMD_QUAD: begin
+                if (bit_counter == 8)
+                    fsm_next_state = FSM_SEND_ADDR;
+                else
+                    fsm_next_state = FSM_SEND_CMD_QUAD;
             end
 
             FSM_SEND_ADDR: begin
@@ -172,7 +237,8 @@ module spi_master (
             initialized <= 1'b0;
             init_cnt <= 12'b0;
 
-            in_continuous_mode <= 1'b0;
+            flash_in_cont_mode <= 1'b0;
+            ram_in_quad_mode <= 1'b0;
         end else begin
 
             if (spi_clk_en) begin
@@ -195,7 +261,8 @@ module spi_master (
                         if (initialized) begin
                             spi_cs_n <= 1'b0;
                             spi_io_oe <= 4'b1111;       // All IOs are outputs for command/address
-                            if (is_instr && in_continuous_mode) begin
+                            if (is_instr) begin
+                                // In this case, the flash should already be in continuous mode
                                 shift_reg_out <= {addr, 8'h00};   // Load address
                             end else begin
                                 shift_reg_out <= cmd_addr;   // Load command and address
@@ -213,68 +280,112 @@ module spi_master (
                     init_cnt <= init_cnt + 1;
                 end
 
-                FSM_RESET: begin
-                    if (bit_counter == 0) begin
-                        // Start reset transaction
-                        spi_cs_n <= 1'b0;
-                        spi_io_oe <= 4'b1111;       // All IOs are outputs
-                        spi_io_out <= 4'b0000;
-                        spi_clk_en <= 1'b0;
+                FSM_RESET_FLASH0: begin
+                    // Start reset transaction
+                    spi_cs_n <= 1'b0;
+                    spi_io_oe <= 4'b1111;       // All IOs are outputs
+                    spi_io_out <= 4'b0000;
+                    spi_clk_en <= 1'b0;
 
-                        write_mosi <= 1'b1;
+                    write_mosi <= 1'b1;
+                end
 
+                FSM_RESET_FLASH1: begin
+                    // Send 8 0x0 nibbles to make sure flash quit continuous mode
+                    // If flash is not in continuous mode, it receives the 0x00 command => do nothing
+                    // If flash is in continuous mode, it receives the 0x000000 address
+                    // and 0x00 for M[7:0] => quit continuous mode
+                    spi_cs_n <= 1'b0;
+                    spi_clk_en <= 1'b1;
+
+                    spi_io_oe <= 4'b1111;       // All IOs are outputs
+                    spi_io_out <= 4'b0000;
+
+                    if (write_mosi == 1'b1) begin
                         bit_counter <= bit_counter + 1;
-                    end else if (bit_counter < 9) begin
-                        // Send 8 0x0 nibbles to make sure flash quit continuous mode
-                        // If flash is not in continuous mode, it receives the 0x00 command => do nothing
-                        // If flash is in continuous mode, it receives the 0x000000 address
-                        // and 0x00 for M[7:0] => quit continuous mode
-                        spi_cs_n <= 1'b0;
-                        spi_clk_en <= 1'b1;
+                    end
+                    write_mosi <= ~write_mosi;
+                end
 
-                        spi_io_oe <= 4'b1111;       // All IOs are outputs
-                        spi_io_out <= 4'b0000;
+                FSM_RESET_FLASH2: begin
+                    // Release bus and wait for some cycles
+                    spi_cs_n <= 1'b0;
+                    spi_clk_en <= 1'b1;
+                    spi_io_oe <= 4'b0000;       // Release bus
 
-                        if (write_mosi == 1'b1) begin
-                            bit_counter <= bit_counter + 1;
-                        end
-                        write_mosi <= ~write_mosi;
-                    end else if (bit_counter < 14) begin
-                        // Release bus and wait for some cycles
-                        spi_cs_n <= 1'b0;
-                        spi_clk_en <= 1'b1;
-                        spi_io_oe <= 4'b0000;       // Release bus
+                    if (write_mosi == 1'b1) begin
+                        bit_counter <= bit_counter + 1;
+                    end
+                    write_mosi <= ~write_mosi;
+                end
 
-                        if (write_mosi == 1'b1) begin
-                            bit_counter <= bit_counter + 1;
-                        end
-                        write_mosi <= ~write_mosi;
-                    end else if (bit_counter == 14) begin
-                        // Finish reset transaction
-                        initialized <= 1'b1;
+                FSM_RESET_FLASH3: begin
+                    // Finish reset transaction
+                    initialized <= 1'b1;
+                    spi_cs_n <= 1'b1;
+                    spi_clk_en <= 1'b0;
+                end
+
+                FSM_RESET_FLASH4: begin
+                    // Prepare for fetch instruction
+                    spi_cs_n <= 1'b0;
+                    spi_io_oe <= 4'b1111;       // All IOs are outputs for command/address
+                    // Fetch instruction for first time, need both command and address
+                    shift_reg_out <= cmd_addr;
+                    shift_reg_in <= 32'b0;
+                    is_write_op <= write_enable; // Store operation type
+
+                    spi_clk_en <= 1'b0;
+                    bit_counter <= 6'b0;
+                    spi_io_out <= 4'b0000;
+
+                    write_mosi <= 1'b1;
+                end
+
+                FSM_RESET_RAM0: begin
+                    // Start reset transaction
+                    spi_cs_n <= 1'b0;
+                    spi_io_oe <= 4'b1111;       // All IOs are outputs
+                    spi_io_out <= 4'b0000;
+                    spi_clk_en <= 1'b0;
+                    bit_counter <= 0;
+
+                    write_mosi <= 1'b1;
+                end
+
+                FSM_RESET_RAM1: begin
+                    if (bit_counter == 8) begin
                         spi_cs_n <= 1'b1;
                         spi_clk_en <= 1'b0;
-                        bit_counter <= bit_counter + 1;
-                    end else if (bit_counter == 15) begin
-                        // Prepare for fetch instruction
+                        ram_in_quad_mode <= 1'b1;
+                    end else begin
                         spi_cs_n <= 1'b0;
-                        spi_io_oe <= 4'b1111;       // All IOs are outputs for command/address
-                        if (is_instr && in_continuous_mode) begin
-                            shift_reg_out <= {addr, 8'h00};   // Load address
-                        end else begin
-                            shift_reg_out <= cmd_addr;   // Load command and address
+                        spi_clk_en <= 1'b1;
+                        if (write_mosi == 1'b1) begin  // Falling edge of SPI clock
+                            spi_io_out <= {3{cmd_enter_quad_mode[bit_counter]}};
+                            bit_counter <= bit_counter + 1;
                         end
-                        shift_reg_in <= 32'b0;
-                        is_write_op <= write_enable; // Store operation type
 
-                        spi_clk_en <= 1'b0;
-                        bit_counter <= 6'b0;
-                        spi_io_out <= 4'b0000;
-
-                        write_mosi <= 1'b1;
+                        write_mosi <= ~write_mosi;
                     end
                 end
-                
+
+                FSM_RESET_RAM2: begin
+                    // Prepare for fetch instruction
+                    spi_cs_n <= 1'b0;
+                    spi_io_oe <= 4'b1111;       // All IOs are outputs for command/address
+                    // Fetch instruction for first time, need both command and address
+                    shift_reg_out <= cmd_addr;
+                    shift_reg_in <= 32'b0;
+                    is_write_op <= write_enable; // Store operation type
+
+                    spi_clk_en <= 1'b0;
+                    bit_counter <= 6'b0;
+                    spi_io_out <= 4'b0000;
+
+                    write_mosi <= 1'b1;
+                end
+
                 FSM_SEND_CMD: begin
                     spi_clk_en <= 1'b1;
                     spi_cs_n <= 1'b0;
@@ -282,6 +393,22 @@ module spi_master (
                         spi_io_out <= {3'b0, shift_reg_out[31]};
                         shift_reg_out <= {shift_reg_out[30:0], 1'b0};
                         bit_counter <= bit_counter + 1;
+                    end
+
+                    if (bit_counter == 8) begin
+                        bit_counter <= 6'b0;  // Reset counter for data phase
+                    end
+
+                    write_mosi <= ~write_mosi;
+                end
+
+                FSM_SEND_CMD_QUAD: begin
+                    spi_clk_en <= 1'b1;
+                    spi_cs_n <= 1'b0;
+                    if (write_mosi == 1'b1) begin  // Falling edge of SPI clock
+                        spi_io_out <= shift_reg_out[31:28];
+                        shift_reg_out <= {shift_reg_out[27:0], 4'b0000};
+                        bit_counter <= bit_counter + 4;
                     end
 
                     if (bit_counter == 8) begin
@@ -328,7 +455,7 @@ module spi_master (
 
                     if (bit_counter == 6) begin
                         bit_counter <= 6'b0;
-                        in_continuous_mode <= 1'b1;
+                        flash_in_cont_mode <= 1'b1;
                     end
 
                     write_mosi <= ~write_mosi;
